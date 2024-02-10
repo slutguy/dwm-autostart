a768722c9d7f88d00c46b916a7cb7a5efa133bda
slutguy <voltgit@gmail.com>
2024-02-11 00:01:52 +0800

    dwmsetting

---
+++ autostart.sh
@@ -0,0 +1,11 @@
+#xrandr --output HDMI-1 --rotate left &
+#feh --bg-fill /home/reap/Pictures/Static/bg/114860776_p0_master1200.jpg 
+feh --bg-fill /home/reap/Pictures/Static/bg/ん、ついてきて.jpg
+picom -b
+tlp -start
+#amixer sset Master 3%+ unmute &
+xmodmap ~/.xmodmap &
+slstatus &
+./Templates/Meta/clash -f ~/Templates/Meta/1.yaml &
+
+
